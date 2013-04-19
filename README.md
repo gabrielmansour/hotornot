213c7a1c26e0339487fa8c7005e08c03147fcfa0
Gabriel Mansour <gabrielmansour@gmail.com>
2013-04-19 11:15:08 -0400

    Initial commit

--- README.md
+++
@@ -1,4 +0,0 @@
-hotornot
-========
-
-A Hot-Or-Not engine to help you rate and discover new dishes and recipes
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hotornot::Application.config.secret_token = '19984bae4941ca304f80823466bc4fefde53610cb3ce0e25222939a50a099852aa1f17762f4f5234a0d10f9be39ee4d54f2e6ca9a946c44dd29e5351fbcd9aed'

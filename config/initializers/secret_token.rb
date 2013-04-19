ebab1b69f8104528c23a5fcf710ac0cf21750fcd
Gabriel Mansour <gabrielmansour@gmail.com>
2013-04-19 11:30:02 -0400

    use latest version of rails

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Hotornot::Application.config.secret_token = '19984bae4941ca304f80823466bc4fefde53610cb3ce0e25222939a50a099852aa1f17762f4f5234a0d10f9be39ee4d54f2e6ca9a946c44dd29e5351fbcd9aed'
+Hotornot::Application.config.secret_token = '292f4c08acb938e2bc98db5497ec6bed39fb6cc3a8b16ab910660d30e77ae15882de1e2bdf40644616893f5fa0f9f6f99281bde5b9dfad2f6f3fd0dd8a5977a0'
